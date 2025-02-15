9c0f93e6089395ecd0975c9b96aa6669bd52bc08
gaailiunas <gailiunaslaurynas@tutanota.com>
2025-02-15 20:03:30 +0200

    feat: authorizing, TODO: requires proper responses

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,61 @@
-fn main() {
-    println!("flowmesh-client");
+use bincode;
+use hex;
+use serde::{Deserialize, Serialize};
+use std::io;
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpSocket,
+};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct AuthorizationPacket {
+    token: [u8; 32],
+    /* more data... */
+}
+
+const AUTHTOKEN: &str = "1a413d012682eb10342cdf7f0e33dd61c2b20e79e4c23feba399919f76d5b408";
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let addr = "127.0.0.1:1081".parse().unwrap();
+
+    let socket = TcpSocket::new_v4()?;
+    let mut stream = socket.connect(addr).await?;
+
+    println!("connected to the server! {:?}", stream);
+
+    let token_arr: [u8; 32] = hex::decode(AUTHTOKEN).unwrap().try_into().unwrap();
+    let authp = AuthorizationPacket { token: token_arr };
+
+    println!("authp: {:?}", authp);
+
+    let serialized = bincode::serialize(&authp).unwrap();
+    println!("serialized: {:?}", serialized);
+    let _ = stream.write(&serialized).await?;
+
+    let mut buf = [0u8; 1024];
+    let n = stream.read(&mut buf).await?;
+    if n == 0 {
+        return Err(io::Error::new(
+            io::ErrorKind::UnexpectedEof,
+            "Connection closed by peer",
+        ));
+    }
+    // check if we have been authorized
+
+
+
+    /*loop {
+        let n = stream.read(&mut buf).await?;
+        if n == 0 {
+            return Err(io::Error::new(
+                io::ErrorKind::UnexpectedEof,
+                "Connection closed by peer",
+            ));
+        }
+
+        println!("n: {n}");
+    }*/
+
+    Ok(())
 }
--- server/src/provider/server.rs
+++ server/src/provider/server.rs
@@ -1,8 +1,70 @@
-use tokio::net::TcpListener;
+use tokio::{io::AsyncReadExt, net::{TcpListener, TcpStream}};
+use hex;
+use bincode;
+use serde::{Serialize, Deserialize};
+
+const AUTHTOKEN: &str = "1a413d012682eb10342cdf7f0e33dd61c2b20e79e4c23feba399919f76d5b408";
+
+#[derive(Serialize, Deserialize, Debug)]
+struct AuthorizationPacket {
+    token: [u8; 32],
+    /* more data... */
+}
+
+enum ProviderPassedState {
+    None,
+}
+
+async fn process(mut socket: TcpStream) {
+    println!("accepted a provider");
+    let mut buf = [0u8; 1024];
+    let passed = ProviderPassedState::None;
+    loop {
+        let n = match socket.read(&mut buf).await {
+            Ok(0) => {
+                println!("provider socket has disconnected");
+                return;
+            }
+            Ok(n) => n,
+            Err(e) => {
+                eprintln!("failed to read from provider socket, err: {e}");
+                return;
+            }
+        };
+
+        println!("read {n} bytes from provider");
+
+        match passed {
+            ProviderPassedState::None => {
+                let authp: AuthorizationPacket = match bincode::deserialize(&buf[..n]) {
+                    Ok(data) => data,
+                    Err(_) => {
+                        println!("failed to deserialize provider data");
+                        return;
+                    }
+                };
+
+                println!("authp: {:?}", authp);
+                if hex::encode(authp.token) == AUTHTOKEN {
+                    println!("got correct auth token");
+                    /* TODO: respond accordingly */
+                }
+                else {
+                    println!("incorrect auth token");
+                    /* TODO: respond accordingly */
+                    return;
+                }
+            }
+        };
+    }
+}
 
 pub async fn run(addr: &str) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let listener = TcpListener::bind(addr).await?;
-    //loop {
-    //}
-    Ok(()) // temporary
+    loop {
+        let (socket, _) = listener.accept().await?;
+        tokio::spawn(async move {
+            process(socket).await;
+        });
+    }
 }
